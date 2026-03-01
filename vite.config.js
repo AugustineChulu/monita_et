50259deee36e63e46f22f5c40d8cbe43129d691d
AugustineChulu <chuluaugustine@gmail.com>
2026-03-01 09:30:05 +0200

    testing url change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig(() => {
-  const projectName = 'monita_et';
+  const projectName = 'test_monita_et';
   const outDir = process.env.GITHUB_ACTIONS ? 'dist' : `dist/${projectName}`;
 
   return {
